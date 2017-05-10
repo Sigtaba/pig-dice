24354f7c857eec909b5e1554f48420e2d99cd7bf
Brendan Hanna and Isabella Abatgis <isabella@>
2017-05-10 16:22:04 -0700

    adds up players scores into total score and changes turns once a player presses hold

--- js/scripts.js
+++ js/scripts.js
@@ -20,13 +20,15 @@ Game.prototype.getDice = function () {
 Player.prototype.getScore = function (num){
   if (num === 1) {
     this.playerTurnScore = 0
-
-    // this.turn = false
   } else {
     this.playerTurnScore += num;
   };
 };
 
+Player.prototype.getTotalScore = function () {
+  this.playerTotalScore += this.playerTurnScore
+  return this.playerTotalScore
+}
 
 //Front End
 $(document).ready(function(playerNumber) {
@@ -44,23 +46,44 @@ $(document).ready(function(playerNumber) {
     if (player1turn === true) {//if player 1 is active, which it will be when the page is loaded, then...
       player1.getScore(currentGame.activeRoll);//random number is stored in player1.playerTurnScore
       $("#turnScore").text(player1.playerTurnScore);//outputs player 1 turnScore to span id = "turnScore"
+      //if statement within if statement
+      console.log(player1);
       if (currentGame.activeRoll === 1) {//if player 1 has rolled a 1, then...
         player1turn = false;//makes player 2 the active player
         $("#playerId").text("Player 2");//tells the user player 2 is active
-      };//end of player 1s turn
+      };//end if statement within if statement
+      //end if statement
     } else if (player1turn === false) {//if you click hit when it's player 2's turn...
-        player2.getScore(currentGame.activeRoll);//random number is stored in player2.playerTurnScore
-        $("#turnScore").text(player2.playerTurnScore);//random number is displayed to user
-        if (currentGame.activeRoll === 1) {//if player 2 rolls a 1...
-          player1turn = true;//switch to player 1's turn
-          $("#playerId").text("Player 1");//tell user it's player 1's turn
-      }
-
-
-
-    };
-
+      player2.getScore(currentGame.activeRoll);//random number is stored in player2.playerTurnScore
+      $("#turnScore").text(player2.playerTurnScore);//random number is displayed to user
+      //if statement within if statement
+      if (currentGame.activeRoll === 1) {//if player 2 rolls a 1...
+        player1turn = true;//switch to player 1's turn
+        $("#playerId").text("Player 1");//tell user it's player 1's turn
+      };//end if statement within if statement
+    };//else statement
 
   });//end click function
+
+  $("#hold").click(function() {//when you click hold
+    // console.log(player1.getTotalScore());
+    console.log(player1);
+    if (player1turn === true) {
+      console.log(player1);
+      // console.log(parseInt(player1.playerTurnScore) + parseInt(player1.player1TotalScore));
+      // (player1.getTotalScore());
+      // console.log(player1);
+      // console.log(player1.playerTotalScore);
+      $("#totalScore1").text(player1.getTotalScore());
+      player1turn = false;//makes player 2 the active player
+      $("#playerId").text("Player 2");//tells the user player 2 is active
+
+    }
+    else if (player1turn === false) {
+      $("#totalScore2").text(player2.getTotalScore());
+      player1turn = true;//makes player 1 the active player
+      $("#playerId").text("Player 1");//tells the user player 1 is active
+    }
+  });
 
 });//end document ready
