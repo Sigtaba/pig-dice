66962cac5f7b55c3db0d5e700e239e429483b162
Brendan Hanna and Isabella Abatgis <isabella@>
2017-05-10 10:39:45 -0700

    add dice number to players turn score

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,30 @@
 //Back End
 
-function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+function Player(name, turnScore, totalScore) {
+  this.playerName = name;
+  this.playerTurnScore = turnScore;
+  this.playerTotalScore = totalScore;
 }
 
+Player.prototype.getDice = function () {
+  this.playerTurnScore += Math.floor(Math.random() * 6) + 1;
+}
 
-
-
+function addRoll(currentRoll, currentPlayer){
+  currentRoll + currentPlayer.turnScore;
+}
 
 //Front End
 $(document).ready(function() {
+  var player1 = new Player("player1", 0, 0);
+  var player2 = new Player("player2", 0, 0);
+
   $("#hit").click(function() {
-    var diceNumber = getRandomIntInclusive(1,6);
-    console.log(diceNumber);
+    // player.getDice();
+    player1.getDice();
+
+    console.log(player1);
+
   });
+
 });
