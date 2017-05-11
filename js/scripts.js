65bd045726a1ca66db52fd51906c257efb864701
Brendan Hanna and Isabella Abatgis <isabella@>
2017-05-11 13:30:58 -0700

    add dice image and css styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,24 @@
 //Back End
 
+
 function Player(name, turnScore, totalScore) {
   this.playerName = name;
   this.playerTurnScore = turnScore;
   this.playerTotalScore = totalScore;
 }
 
-function Game(activeName, activeRoll) {
+function Game(activeName, activeRoll, activeSecondRoll) {
   this.gameName = activeName;
   this.activeRoll = activeRoll;
+  this.activeSecondRoll = activeSecondRoll;
 }
 
 Game.prototype.getDice = function () {
   this.activeRoll = Math.floor(Math.random() * 6) + 1;
+}
+
+Game.prototype.getSecondDice = function () {
+  this.activeSecondRoll = Math.floor(Math.random() * 6) + 1;
 }
 
 Player.prototype.getScore = function (num){
@@ -33,20 +39,20 @@ Player.prototype.getTotalScore = function () {
 $(document).ready(function(playerNumber) {
   var player1 = new Player("player1", 0, 0);
   var player2 = new Player("player2", 0, 0);
-  var currentGame = new Game("game1", 0);
+  var currentGame = new Game("game1", 0, 0);
   var player1turn = true;
 
   $("#playerId").text("Player 1");//display "player 1" to user
 
   $("#hit").click(function() {//when you click "hit"
     currentGame.getDice();//generates random number and stores it in currentGame.activeRoll
-    $("#diceId").text(currentGame.activeRoll);//displays that random number in span id = diceId
+    // $("#diceId").text("<img src='img/pig.jpg'>");//displays that random number in span id = diceId
+        $("#dice-pic").show();
 
     if (player1turn === true) {//if player 1 is active, which it will be when the page is loaded, then...
       player1.getScore(currentGame.activeRoll);//random number is stored in player1.playerTurnScore
       $("#turnScore").text(player1.playerTurnScore);//outputs player 1 turnScore to span id = "turnScore"
       //if statement within if statement
-      console.log(player1);
       if (currentGame.activeRoll === 1) {//if player 1 has rolled a 1, then...
         player1turn = false;//makes player 2 the active player
         $("#playerId").text("Player 2");//tells the user player 2 is active
@@ -61,6 +67,27 @@ $(document).ready(function(playerNumber) {
         $("#playerId").text("Player 1");//tell user it's player 1's turn
       };//end if statement within if statement
     };//else statement
+
+    if (currentGame.activeRoll === 2) {
+      $("#diceImage").attr( "src", "img/2.png");
+    }
+    else if (currentGame.activeRoll === 3) {
+      $("#diceImage").attr( "src", "img/3.png");
+    }
+    else if (currentGame.activeRoll === 4) {
+      $("#diceImage").attr( "src", "img/4.png");
+    }
+    else if (currentGame.activeRoll === 5) {
+      $("#diceImage").attr( "src", "img/5.png");
+    }
+    else if (currentGame.activeRoll === 6) {
+      $("#diceImage").attr( "src", "img/6.png");
+    }
+    else {
+      $("#diceImage").attr( "src", "img/1.png");
+    }
+
+
 
   });//end click function
 
@@ -88,7 +115,8 @@ $(document).ready(function(playerNumber) {
     }
     $("#turnScore").text("");
     $("#diceId").text("");
+    $("#diceImage").attr( "src", "img/none.png");
 
-  });
+  });//end hold click function
 
 });//end document ready
